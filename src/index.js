43aa9f142412715fa5b76519a4750052feb63bc2
olifur <olifur@domain.net>
2022-07-26 10:32:10 +0200

    adding avax and avax_testnet to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { CONSTANTS as ETHEREUM } from './ethereum'
 import { CONSTANTS as BSC } from './bsc'
 import { CONSTANTS as POLYGON } from './polygon'
 import { CONSTANTS as SOLANA } from './solana'
+import { CONSTANTS as AVAX } from './avax'
+import { CONSTANTS as AVAX_TESTNET } from './avax_testnet'
+
+
 
 let CONSTANTS = {}
 
@@ -9,5 +13,7 @@ CONSTANTS['ethereum'] = ETHEREUM
 CONSTANTS['bsc'] = BSC
 CONSTANTS['polygon'] = POLYGON
 CONSTANTS['solana'] = SOLANA
+CONSTANTS['avax'] = AVAX
+CONSTANTS['avax_testnet'] = AVAX_TESTNET
 
 export { CONSTANTS }
